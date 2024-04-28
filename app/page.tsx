5735345e2326df4366fc35d7afb8973f95562094
Aditya <adityabxr30@gmail.com>
2024-04-28 14:50:24 +0530

    Suspense Boundrary Error Fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React, { Suspense } from "react";
 import NewsCard from "@/components/NewsCard";
 import Pagination from "@/components/Pagination";
 
-async function getNews(slug?: string,page?:string | undefined | string[]) {
+async function getNews(slug?: string, page?: string | undefined | string[]) {
   const res = await fetch(
-      `https://newsapi.org/v2/top-headlines?country=in&sortBy=popularity&apiKey=${process.env.NEXT_PUBLIC_API_KEY}&pageSize=18&page=${page}`
+    `https://newsapi.org/v2/top-headlines?country=in&sortBy=popularity&apiKey=${process.env.NEXT_PUBLIC_API_KEY}&pageSize=18&page=${page}`
   );
   const data = await res.json();
-  const news = data.articles.filter((item: any) => item.urlToImage);
-  const pageLength = Math.ceil(data.totalResults/18);
-  return {news,pageLength};
+  const news = data.articles;
+  const pageLength = Math.ceil(data.totalResults / 18);
+  return { news, pageLength };
 }
 
 export default async function Home(): Promise<React.ReactElement> {
-  const {news,pageLength} = await getNews();
+  const { news, pageLength } = await getNews();
 
   return (
     <main className="flex flex-col items-center gap-3">
@@ -23,7 +23,9 @@ export default async function Home(): Promise<React.ReactElement> {
             return <NewsCard item={item} key={item.title} />;
           })}
       </div>
-      <Pagination pageLength={pageLength} slug="/" />
+      <Suspense fallback={null}>
+        <Pagination pageLength={pageLength} slug="/" />
+      </Suspense>
     </main>
   );
 }
